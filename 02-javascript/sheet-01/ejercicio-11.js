6caaabdb4a4d8fb790f252f20cde7bf08464807e
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-26 18:18:10 +0100

    feat: add ejercicio 11

---
+++ 02-javascript/sheet-01/ejercicio-11.js
@@ -0,0 +1,6 @@
+console.log("5" + 2); // Concatenación de cadena y número, el resultado es "52"
+console.log("5" - 2); // Resta, el resultado es 3 (el operador "-" convierte la cadena "5" a número)
+console.log(10 + true); //Concatenación de número y booleano, el resultado es 11 (el operador "+" convierte el booleano true a 1)
+console.log(10 + false); //Concatenacion de número y booleano, el resultado es 10 (el operador "+" convierte el booleano false a 0)
+console.log("10" === 10); // Comparación estricta, el resultado es false (diferente tipo de datos)
+console.log("10" == 10); // Comparación no estricta, el resultado es true (el operador "==" convierte la cadena "10" a número antes de comparar)
