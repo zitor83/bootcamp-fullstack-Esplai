9c1cc61a059babc1ceb75919171dd33c0a6a8c61
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-26 13:26:33 +0100

    feat: comprobar tipos de datos con typeof

---
+++ 02-javascript/sheet-01/ejercicio-02.js
@@ -0,0 +1,11 @@
+const nombre = "Jose";
+const numeroRuedasCoche = 4;
+const esMayorDeEdad = true;
+let ciudad;
+let numeroHijos = null;
+
+console.log(`Valor: ${nombre} | Tipo: ${typeof nombre}`);
+console.log(`Valor: ${numeroRuedasCoche} | Tipo: ${typeof numeroRuedasCoche}`);
+console.log(`Valor: ${esMayorDeEdad} | Tipo: ${typeof esMayorDeEdad}`);
+console.log(`Valor: ${ciudad} | Tipo: ${typeof ciudad}`);
+console.log(`Valor: ${numeroHijos} | Tipo: ${typeof numeroHijos}`);
