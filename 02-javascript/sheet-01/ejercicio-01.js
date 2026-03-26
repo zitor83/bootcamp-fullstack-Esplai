15b6100d202e3e5d71b0ea446b852861a52c1705
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-26 13:19:30 +0100

    feat: crear ficha personal con variables y template literals

---
+++ 02-javascript/sheet-01/ejercicio-01.js
@@ -0,0 +1,6 @@
+const nombre = "Jose";
+let edad = 42;
+const ciudad = "Málaga";
+const programmingExperience=true;
+
+console.log(`Hola, me llamo ${nombre}, tengo ${edad}, vivo en ${ciudad} y mi experiencia previa en programación es: ${programmingExperience}`);
