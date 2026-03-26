fc2bedb8db2c3fc24a1e736635960e7ce3da8d66
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-26 13:53:07 +0100

    feat: add ejercicio-04: product purchase calculation with total cost display

---
+++ 02-javascript/sheet-01/ejercicio-04.js
@@ -0,0 +1,6 @@
+let nombreProducto = "Camiseta";
+let precioUnitario = 19.99;
+let cantidad = 3;
+let costeEnvio = 4.99;
+
+console.log(`Has comprado ${cantidad} unidades de "${nombreProducto}". Subtotal: ${precioUnitario * cantidad} €. Envio: ${costeEnvio} €. Total: ${precioUnitario * cantidad + costeEnvio} €.`);
