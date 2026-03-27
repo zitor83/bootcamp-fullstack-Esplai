c92356001c7f534f71e23a9f1b4ea949ae022ea1
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-27 12:25:09 +0100

    feat:add ejercicio-06

---
+++ 02-javascript/sheet-02/ejercicio-06.js
@@ -0,0 +1,6 @@
+//Crea una función llamada sumar que reciba dos números y devuelva el resultado de la suma.
+function sumar(a, b) {
+    return a + b;
+}
+const resultado = sumar(5, 3);
+console.log(resultado);
