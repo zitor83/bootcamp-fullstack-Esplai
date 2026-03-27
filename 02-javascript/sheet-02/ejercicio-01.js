d260ba3518ce063a67357def762132292a6f2da9
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-27 11:57:19 +0100

    feat: add ejercicio-01: loops

---
+++ 02-javascript/sheet-02/ejercicio-01.js
@@ -0,0 +1,11 @@
+//Escribe un programa que muestre por consola los números del 1 al 10.
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+let j = 0;
+while (j <= 10) {
+    console.log(j);
+    j++;
+}
