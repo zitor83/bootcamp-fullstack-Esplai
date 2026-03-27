568dcea5980e4a012f57bf6e388d8cafd9fad385
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-27 12:02:29 +0100

    feat:add ejercicio-02

---
+++ 02-javascript/sheet-02/ejercicio-02.js
@@ -0,0 +1,10 @@
+//Escribe un programa que muestre por consola los números del 10 al 1 en orden descendente.
+for (let i = 10; i >= 0; i--) {
+    console.log(i);
+}
+
+let j = 10;
+while (j >= 0) {
+    console.log(j);
+    j--;
+}
