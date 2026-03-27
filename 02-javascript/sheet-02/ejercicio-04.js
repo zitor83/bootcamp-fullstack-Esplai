d2b9b295e1788b72f73f378aa17cb59b35ae923c
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-27 12:07:02 +0100

    feat:add ejercicio-04

---
+++ 02-javascript/sheet-02/ejercicio-04.js
@@ -0,0 +1,8 @@
+// Pide en el código una variable numero y muestra su tabla de multiplicar del 1 al 10. Lo dejo a
+// tu elección: puedes pedirlo o directamente declararlo tú en el código.
+
+let numero = 5; 
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`${numero} x ${i} = ${numero * i}`);
+}
