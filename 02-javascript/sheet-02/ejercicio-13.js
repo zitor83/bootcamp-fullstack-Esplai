2e059164b18a5be942d89e91ca24819449a3c077
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-27 13:34:38 +0100

    feat:add ejercicio-13

---
+++ 02-javascript/sheet-02/ejercicio-13.js
@@ -0,0 +1,13 @@
+//Define una función que reciba un número y devuelva su cuadrado.
+//  Escríbela de tres maneras:
+function cuadrado1(num) {
+    return num * num;
+}
+
+const cuadrado2 = function(num) {
+    return num * num;
+};
+
+const cuadrado3 = (num) => {
+    return num * num;
+};
