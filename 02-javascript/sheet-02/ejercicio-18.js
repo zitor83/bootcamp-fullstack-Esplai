384163ac545cb196b7fa5d1c94f3dda5d242c317
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-28 14:40:36 +0100

    feat:refactor organizarTurnos function in ejercicio-18  to include turnoInicial parameter

--- 02-javascript/sheet-02/ejercicio-18.js
+++ 02-javascript/sheet-02/ejercicio-18.js
@@ -1,14 +1,15 @@
 const numeroParticipantes = 0;
 
 function validarParticipantes(numeroParticipantes) {
-   return typeof numeroParticipantes !== "number" || numeroParticipantes <= 0
-    }
+    return typeof numeroParticipantes !== "number" || numeroParticipantes <= 0
+}
 
 
-function generarTextoTurno(numeroTurno, mensajeBase) {
-    return `Turno ${numeroTurno}: ${mensajeBase}`;
+function generarTextoTurno(turnoInicial, mensajeBase) {
+    return `Turno ${turnoInicial}: ${mensajeBase}`;
 
 }
+
 
 function textoEnMayusculas(texto) {
     console.log(texto.toUpperCase());
@@ -18,22 +19,21 @@ function textoEntreGuiones(texto) {
     console.log(`--- ${texto} ---`);
 }
 
-function organizarTurnos(numeroParticipantes, mensajeBase, callback) {
+function organizarTurnos(numeroParticipantes, turnoInicial, mensajeBase, callback) {
     const validacion = validarParticipantes(numeroParticipantes);
     if (validacion) {
         return console.log("El número de participantes debe ser un número mayor que 0 y tener un formato correcto.");
     }
-    for (let i = 1; i <= numeroParticipantes; i++) {
+    for (let i = turnoInicial; i < turnoInicial + numeroParticipantes; i++) {
         const textoTurno = generarTextoTurno(i, mensajeBase);
         callback(textoTurno);
-
     }
 }
 
-organizarTurnos(3, "Es tu turno", textoEnMayusculas);
-organizarTurnos(3, "Es tu turno", textoEntreGuiones);
-organizarTurnos(0, "Es tu turno", textoEnMayusculas);
-organizarTurnos("Patata", "Es tu turno", textoEntreGuiones);
-organizarTurnos(-1, "Es tu turno", textoEnMayusculas);
-organizarTurnos(2, "preparen sus armas", textoEntreGuiones);
-organizarTurnos(2, "preparen sus armas", textoEnMayusculas);
+organizarTurnos(3, 1, "Es tu turno", textoEnMayusculas);
+organizarTurnos(3, 1, "Es tu turno", textoEntreGuiones);
+organizarTurnos(0, 1, "Es tu turno", textoEnMayusculas);
+organizarTurnos("Patata", 1, "Es tu turno", textoEntreGuiones);
+organizarTurnos(-1, 1, "Es tu turno", textoEnMayusculas);
+organizarTurnos(2, 3, "preparen sus armas", textoEntreGuiones);
+organizarTurnos(2, 3, "preparen sus armas", textoEnMayusculas);
