e1bd692f385d7aa349910838f1ea6176a05b23c0
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-30 12:21:51 +0200

    feat: add ejercicio-03

---
+++ 02-javascript/sheet-03/ejercicio-03.js
@@ -0,0 +1,27 @@
+/*
+Para cada caso, decide si usarías array, objeto o array de objetos. Después, justifica tu
+decisión brevemente en comentarios dentro del código o en un texto aparte.
+*/
+
+/*
+1. una lista de nombres de alumnos;
+2. los datos de un producto;
+3. varias tareas con título y estado;
+4. las ciudades visitadas por una persona.
+5. un catalogo de peliculas con titulo, año y genero.
+*/
+
+// 1. una lista de nombres de alumnos;
+// Usaría un array, ya que solo necesito almacenar una lista de nombres sin necesidad de asociar cada nombre a una clave específica.
+//###################################################
+// 2. los datos de un producto;
+// Usaría un objeto, ya que necesito almacenar diferentes propiedades (como nombre, precio, descripción) asociadas a un producto específico.
+//###################################################
+// 3. varias tareas con título y estado;
+// Usaría un array de objetos, ya que cada tarea tiene múltiples propiedades (título y estado) y necesito almacenar varias tareas en una estructura organizada.
+//###################################################
+// 4. las ciudades visitadas por una persona.
+// Usaría un array, ya que solo necesito almacenar una lista de ciudades sin necesidad de asociar cada ciudad a una clave específica.
+//###################################################
+// 5. un catalogo de peliculas con titulo, año y genero.
+// Usaría un array de objetos, ya que cada película tiene múltiples propiedades (título, año y género) y necesito almacenar varias películas en una estructura organizada.
