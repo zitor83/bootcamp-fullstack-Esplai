71a264aa49daf37b15c7572563a3338038f5d7a2
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-30 12:15:58 +0200

    feat: add ejercicio-02

---
+++ 02-javascript/sheet-03/ejercicio-02.js
@@ -0,0 +1,15 @@
+const usuario={
+    nombre: "Juan",
+    edad: 30,
+    ciudad: "Madrid",
+    experienciaProgramacion: true,
+    lenguajeFavorito: "JavaScript"
+}
+
+console.log(usuario)
+
+console.log(usuario.nombre)
+console.log(usuario.edad)
+
+usuario.marcaFavorita= "Apple"
+console.log(usuario.marcaFavorita)
