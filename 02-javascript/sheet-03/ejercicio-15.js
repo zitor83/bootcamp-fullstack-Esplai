dab523feceb77b6db4e73e18a1426ff9dcbecd6c
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-30 13:40:01 +0200

    feat: add ejercicio-15

---
+++ 02-javascript/sheet-03/ejercicio-15.js
@@ -0,0 +1,9 @@
+const perfil ={
+    nombre: "Juan",
+    // telefono: undefined,
+    // email:undefined
+}
+
+console.log(perfil.nombre);
+console.log(perfil.telefono?.movil);
+console.log(perfil.email.personal);
