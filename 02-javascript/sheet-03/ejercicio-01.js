41b4c7193e7ed86ea771595a5040bc122e155bcf
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-30 12:10:30 +0200

    feat: add ejercicio-01

---
+++ 02-javascript/sheet-03/ejercicio-01.js
@@ -0,0 +1,6 @@
+const herramientasDesarrollo=["VS code","IntelliJ","Eclipse","NetBeans","Workbench"];
+
+console.log(herramientasDesarrollo);
+console.log(herramientasDesarrollo[0]);
+console.log(herramientasDesarrollo[4]);
+console.log(herramientasDesarrollo.length);
