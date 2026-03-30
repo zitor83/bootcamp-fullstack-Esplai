f400093aeb8ea595fe70690853d42ed7f4adb914
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-30 12:40:17 +0200

    feat: add ejercicio-07

---
+++ 02-javascript/sheet-03/ejercicio-07.js
@@ -0,0 +1,33 @@
+const videojuego = {
+    id: 1,
+    titulo: "The Legend of Zelda: Breath of the Wild",
+    anio: 2017,
+    genero: "Aventura",
+    disponible: true,
+}
+
+const videojuego2 = {
+    id: 2,
+    titulo: "Super Mario Odyssey",
+    anio: 2017,
+    genero: "Plataformas",
+    disponible: true,
+}
+
+const videojuego3 = {
+    id: 3,
+    titulo: "Red Dead Redemption 2",
+    anio: 2018,
+    genero: "Acción",
+    disponible: true,
+}
+
+const listaVideojuegos = [videojuego, videojuego2, videojuego3]
+
+console.log(listaVideojuegos)
+console.log(listaVideojuegos[1].titulo)
+console.log(listaVideojuegos[2].disponible);
+
+
+
+
