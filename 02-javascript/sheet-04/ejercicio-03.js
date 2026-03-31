80cc60212d4c0a42d53f122d33fd87b15b319b53
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-31 12:37:58 +0200

    feat: add ejercicio-03

---
+++ 02-javascript/sheet-04/ejercicio-03.js
@@ -0,0 +1,10 @@
+const numeros = [1,3,5,7,9]
+
+const numerosDoblados= numeros.map((numero) => {return numero *2});
+
+const numerosCuadrados= numeros.map((numero) => {return numero ** 2});
+
+
+console.log(numeros);
+console.log(numerosDoblados);
+console.log(numerosCuadrados);
