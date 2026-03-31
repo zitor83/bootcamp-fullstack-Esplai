193b731788c86d32c2488c3233667fa581a2c395
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-31 12:19:43 +0200

    feat: add ejercicio-01

---
+++ 02-javascript/sheet-04/ejercicio-01.js
@@ -0,0 +1,39 @@
+/**
+ * Bloque 1. Reconocer la intención del método
+Ejercicio 1. ¿Qué método usarías?
+
+Para cada caso, indica qué método de array te parece más adecuado y explica brevemente por qué: 
+1. obtener solo los productos disponibles; 
+2. convertir una lista de usuarios en una lista de nombres;
+3. saber si existe algún alumno suspendido; 
+4. comprobar si todas las tareas están completadas; 
+5. encontrar un producto por su id ; 
+6. sumar el precio total de una cesta; 
+7. recorrer una lista para mostrar mensajes por consola. 
+*/
+
+
+//1. Para obtener solo los productos disponibles, usaría el método `filter()`, 
+// ya que permite crear un nuevo array con todos los elementos que cumplan una condición específica,
+//  en este caso, que el producto esté disponible.
+
+//2. Para convertir una lista de usuarios en una lista de nombres, usaría el método `map()`,
+// ya que permite crear un nuevo array con los resultados de aplicar una función a cada elemento del array original.
+
+//3. Para saber si existe algún alumno suspendido, usaría el método `some()`,
+// ya que permite comprobar si al menos un elemento del array cumple con una condición específica, 
+
+//4. Para comprobar si todas las tareas están completadas, usaría el método `every()`,
+// ya que permite comprobar si todos los elementos del array cumplen con una condición específica.
+
+//5. Para encontrar un producto por su id, usaría el método `find()`,
+// ya que permite devolver el primer elemento del array que cumpla con una condición específica, 
+// en este caso, que el id del producto coincida con el id buscado.
+
+//6. Para sumar el precio total de una cesta, usaría el método `reduce()`,
+// ya que permite aplicar una función a un acumulador y a cada elemento del array para reducirlo a un único valor, 
+// en este caso, la suma total de los precios.
+
+//7. Para recorrer una lista y mostrar mensajes por consola, usaría el método `forEach()`,
+// ya que permite ejecutar una función para cada elemento del array, 
+// lo que es útil para realizar acciones como mostrar mensajes por consola sin necesidad de crear un nuevo array.
