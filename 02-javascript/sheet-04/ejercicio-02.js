30eb68ff2bed889331a9355095e9ce62b32469ab
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-03-31 12:30:38 +0200

    feat: add ejercicio-02

---
+++ 02-javascript/sheet-04/ejercicio-02.js
@@ -0,0 +1,11 @@
+const herramientasParaProgramar = [
+    "VS Code",
+    "GitHub",
+    "Node.js",
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "React",
+]
+
+herramientasParaProgramar.forEach((herramienta, indice) => { console.log(`Posición ${indice}: ${herramienta}`) })
