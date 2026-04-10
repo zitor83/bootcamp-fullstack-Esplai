209635e61bc7d978238aa8ac7e27797e49046754
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-04-10 13:36:43 +0200

    feat: add WelcomeReact component for exercise 3

--- 03-react/sheet-01/src/App.tsx
+++ 03-react/sheet-01/src/App.tsx
@@ -1,12 +1,14 @@
 
 import './App.css'
-import WelcomeCard from './components/WelcomeCard'
+//import WelcomeCard from './components/WelcomeCard'
+import WelcomeReact from './components/WelcomeReact'
 
 function App() {
   return (
     <div>
       <h1>React Sheet 01</h1> 
-      <WelcomeCard />
+      {/* <WelcomeCard /> */}
+      <WelcomeReact />
     </div>
   
 )}
---
+++ 03-react/sheet-01/src/components/WelcomeReact.tsx
@@ -0,0 +1,12 @@
+function WelcomeReact() {
+    return (
+        <section className="hero">
+<h1>React desde cero</h1>
+<p />Aprende a crear interfaces modernas.
+<img src="https://via.placeholder.com/200" alt="Portada" />
+<button>Empezar</button>
+</section>
+);
+}
+
+export default WelcomeReact;
