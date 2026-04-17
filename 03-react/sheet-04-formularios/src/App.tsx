d5f69c9aa311f29a398eb98ebadea16fd5a19ce5
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-04-17 09:07:34 +0200

    feat: wip ejercicios de formularios 1 al 4

--- 03-react/sheet-04-formularios/src/App.tsx
+++ 03-react/sheet-04-formularios/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import MessageToggle from "./components/ex02/MessageToggle";
 import SampleAccess from "./components/ex03/SampleAccess";
 import TextAreaComponent from "./components/ex04/TextAreaComponent";
+import FormValidation from "./components/ex05/FormValidation";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <MessageToggle />
       <SampleAccess />
       <TextAreaComponent />
+      <FormValidation />
     </>
   );
 }
---
+++ 03-react/sheet-04-formularios/src/components/ex05/FormValidation.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function FormValidation() {
+    const [email, setEmail] = React.useState("");
+    const [name, setName] = React.useState("");
+    const [error, setError] = React.useState("");
+
+    const handleSubmit = (e) => {        e.preventDefault();
+        if (name.trim() === "") {
+            setError("El nombre no pude estar vacio.");
+            return;
+        }
+        setError("");
+
+  return (
+    <div>
+      <h1>Validación de Formulario</h1>
+        <form>
+            <label htmlFor="name">Nombre:</label>
+            <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} required />
+
+            <label htmlFor="email">Correo Electrónico:</label>
+            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+
+
+            </form>
+
+            <p>{error}</p>
+    </div>
+  )
+}
+
+export default FormValidation
