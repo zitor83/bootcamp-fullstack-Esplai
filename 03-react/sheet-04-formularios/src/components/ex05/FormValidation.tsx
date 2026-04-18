59c1d9474ccf8fd04474818e6473c9342d02f67f
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-04-18 18:21:23 +0200

    feat: form validation in exercise 05

--- 03-react/sheet-04-formularios/src/components/ex05/FormValidation.tsx
+++ 03-react/sheet-04-formularios/src/components/ex05/FormValidation.tsx
@@ -1,33 +1,75 @@
-import React, { useState } from 'react'
+import React, { useId, useState } from "react";
 
 function FormValidation() {
-    const [email, setEmail] =useState("");
-    const [name, setName] = React.useState("");
-    const [error, setError] = useState("");
+  // Estado para almacenar los valores del formulario
+  const [email, setEmail] = useState("");
+  const [name, setName] = React.useState("");
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {        e.preventDefault();
-        if (name.trim() === "") {
-            setError("El nombre no pude estar vacio.");
-            return;
-        }
-        setError("");
+  // Estado para almacenar el mensaje de validación(guardo el tipo de mensaje(error o exito) y el texto)
+  const [message, setMessage] = useState({ type: "", text: "" });
+
+  //useId para generar IDs únicos para los inputs y sus labels.
+  const idBase = useId();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    //Comprobar que el campo de nombre no esté vacío. Si lo está, se muestra un mensaje de error.
+    if (name.trim() === "") {
+      setMessage({ type: "error", text: "El nombre es obligatorio." });
+      return;
+    }
+    setMessage({ type: "success", text: "Formulario válido." });
+
+    //Comprobar que el campo de correo electrónico contenga un "@" para validar su formato. Si no lo contiene, se muestra un mensaje de error.
+    if (!email.includes("@")) {
+      setMessage({
+        type: "error",
+        text: "El correo electrónico no es válido.",
+      });
+      return;
+    }
+
+    //Si el codigo llega a este punto, significa que ambos campos son válidos, por lo que se muestra un mensaje de éxito y se limpian los campos del formulario.
+    setMessage({ type: "success", text: "Formulario válido." });
+
+    //Opcional. Limpiar los campos del formulario después de una validación exitosa.
+    setName("");
+    setEmail("");
+  };
 
   return (
-    <div>
+    <div
+      style={{ margin: "10px", border: "1px solid #141414", padding: "10px" }}
+    >
       <h1>Validación de Formulario</h1>
-        <form>
-            <label htmlFor="name">Nombre:</label>
-            <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} required />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor={idBase + "-name"}>Nombre:</label>
+        <input
+          type="text"
+          id={idBase + "-name"}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
 
-            <label htmlFor="email">Correo Electrónico:</label>
-            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <label htmlFor={idBase + "-email"}>Correo Electrónico:</label>
+        <input
+          type="email"
+          id={idBase + "-email"}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
 
+        <button type="submit">Enviar</button>
+      </form>
 
-            </form>
+      {/*Zona de mensajes de la interfaz.*/}
 
-            <p>{error}</p>
+      {message.text && <p>{message.text}</p>}
     </div>
-  )
+  );
 }
 
-export default FormValidation
+export default FormValidation;
