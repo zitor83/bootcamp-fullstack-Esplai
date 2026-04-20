3fc20200059a416f3b0baed0d9be199bbcd249a2
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-04-20 21:34:20 +0200

    feat: fetch initial pokemon list on mount using useEffect

--- frontend/mundo-pokemon-react/src/App.tsx
+++ frontend/mundo-pokemon-react/src/App.tsx
@@ -1,121 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+// Definimos la interfaz para los datos básicos de un Pokémon (nombre y URL)
+export interface PokemonBase {
+  name: string;
+  url: string;
 }
 
-export default App
+const POKEMONS_POR_PAGINA = 24;
+
+function App() {
+  // 1. Estado para la lista global de Pokémon  (sin filtrar). Aqui le paso un generico para decirle que es un array de PokemonBase, y el estado inicia como un array vacio.
+  const [listaGlobal, setListaGlobal] = useState<PokemonBase[]>([]);
+
+  // 2. Estado para el texto de búsqueda que el usuario ingresa para filtrar la lista global
+  const [busqueda, setBusqueda] = useState("");
+
+  // 3. Estado para la página actual que el usuario está viendo (inicia en 1)
+  const [paginaActual, setPaginaActual] = useState(1);
+
+  // 4. Estado para los detalles de los Pokémon de la página actual.
+  const [pokemonsEnPantalla, setPokemonsEnPantalla] = useState<any[]>([]);
+
+  // 5. Estado para controlar la pantalla de carga
+  const [cargando, setCargando] = useState(false);
+
+  // USE EFFECT PARA CARGAR LA LISTA GLOBAL DE POKEMONES AL INICIAR LA APLICACION
+  useEffect(() => {
+    //Definimos la función asíncrona dentro del useEffect para cargar la lista global de Pokémon al iniciar la aplicación.
+    const fetchListaInicial = async () => {
+      setCargando(true);
+      try {
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=-1",
+        );
+        const data = await response.json();
+
+        // Guardamos la lista global de Pokémon en el estado. El resultado en data.results es un array de objetos con name y url.
+        setListaGlobal(data.results);
+      } catch (error) {
+        console.error("Error al cargar la lista de Pokémon:", error);
+      } finally {
+        setCargando(false);
+      }
+    };
+
+    // Llamamos a la función para cargar la lista global de Pokémon al iniciar la aplicación.
+    // Al pasar un array vacío como segundo argumento, este useEffect solo se ejecutará una vez al montar el componente, lo que es ideal para cargar datos iniciales.
+    fetchListaInicial();
+  }, []);
+
+  // ==========================================
+  // ESTADOS DERIVADOS (Los calculamos a partir de los estados anteriores)
+  // ==========================================
+
+  // Filtramos la lista global basándonos en el texto de búsqueda
+  const listaFiltrada = listaGlobal.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(busqueda.toLowerCase()),
+  );
+
+  // Calculamos el total de páginas basándonos en la lista ya filtrada
+  const totalPaginas = Math.max(
+    1,
+    Math.ceil(listaFiltrada.length / POKEMONS_POR_PAGINA),
+  );
+
+  return (
+    <main>
+      <h1>Pokédex</h1>
+      {/* Inyecta los componentes aquí */}
+      {cargando ? (
+        <p>Cargando...</p>
+      ) : (
+        <p>Total Pokemons cargados: {listaGlobal.length}</p>
+      )}
+    </main>
+  );
+}
+
+export default App;
