e6994c42aaf170f968a192b7bd54084f669e9055
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-04-21 18:24:05 +0200

    feat: implement getPokemonDetails function to fetch Pokémon data and evolution

---
+++ frontend/mundo-pokemon-react/src/services/api.ts
@@ -0,0 +1,24 @@
+
+import type { PokemonDetail } from "../types/pokemon";
+import { adaptPokemonData } from "../utils/adapters";
+
+
+
+// Función para obtener los detalles de un Pokémon a partir de su URL en la API.
+// Esta función hace dos llamadas a la API: una para obtener los datos básicos del Pokémon y otra para obtener su evolución (si existe).
+export async function getPokemonDetails(urlPokemon: string): Promise<PokemonDetail> {
+    // 1. Pedimos la información del pokemon
+    const response = await fetch(urlPokemon);
+    const dataPokemon = await response.json();
+
+    // 2. Pedimos la evolución que viene anidada en la información de la especie del pokemon
+    const responseSpecies = await fetch(dataPokemon.species.url);
+    const dataSpecies = await responseSpecies.json();
+
+    // 3. Inyectamos la evolución en nuestro objeto de datos del pokemon. Si el pokemon no tiene evolución, le asignamos null.
+    const evolvesFrom = dataSpecies.evolves_from_species
+        ? dataSpecies.evolves_from_species.name
+        : null;
+
+    return adaptPokemonData(dataPokemon, evolvesFrom);
+}
