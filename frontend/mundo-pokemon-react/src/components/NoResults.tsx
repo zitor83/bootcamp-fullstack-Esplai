860c400dcdc80470788ace48e7c376708863b9ef
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-04-26 17:18:50 +0200

    fix: update NoResults component to improve user feedback on search queries

--- frontend/mundo-pokemon-react/src/components/NoResults.tsx
+++ frontend/mundo-pokemon-react/src/components/NoResults.tsx
@@ -6,8 +6,8 @@ function NoResults({ query }: NoResultsProps) {
   return (
     <div className="no-results-container">
       <h2>!No has conseguido atrapar ningún Pokémon!</h2>
+      <p>No se encontraron resultados para la búsqueda "{query}".</p>
       <p> Intentalo de nuevo con otro término de búsqueda.</p>
-      
     </div>
   );
 }
