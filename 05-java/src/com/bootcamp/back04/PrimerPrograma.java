775808b1df642b1f45f96e8bb96002e103dc47bc
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-05-04 14:15:05 +0200

    feat:First Java exercise

---
+++ 05-java/src/com/bootcamp/back04/PrimerPrograma.java
@@ -0,0 +1,8 @@
+package com.bootcamp.back04;
+
+public class PrimerPrograma {
+    public static void main(String[] args) {
+    
+        System.out.println("Bienvenido/a a Java Bootcamp!");
+    }
+}
